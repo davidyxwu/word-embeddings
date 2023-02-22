62522ebc9c63bd806342b4f745136b03b10a9c7c
David Wu <david.yx.wu@gmail.com>
2023-02-21 19:01:27 -0600

    remove code

--- code/train.py
+++
@@ -1,8 +0,0 @@
-from fetch_datasets import fetch_IMDB, fetch_CoLA
-from model import GRU
-from torchsummary import summary
-
-model = GRU(25004, 300, 256, 64, 2)
-
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
